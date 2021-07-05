19015cc912577495257dbad574cd42600b324613
Cheongha <sch7188@gmail.com>
2021-07-05 22:12:35 +0900

    fix: lyrics model
    typescript 사용

--- src/models/lyrics.ts
+++ src/models/lyrics.ts
@@ -1,8 +1,25 @@
-module.exports = (sequelize, DataTypes) => {
-    return sequelize.define('Lyrics', {
+import { BuildOptions, DataTypes, Model, Sequelize } from "sequelize";
+
+export interface LyricsAttributes{
+    kor: string;
+    eng: string;
+    start_time: string;
+    duration: string;
+}
+
+export interface LyricsModel extends Model<LyricsAttributes>, LyricsAttributes{}
+
+export class Lyrics extends Model<LyricsModel, LyricsAttributes>{}
+
+export type LyricsStatic = typeof Model & {
+    new (values?: object, options?: BuildOptions): LyricsModel;
+  };
+
+export function LyricsFactory (sequelize : Sequelize) {
+    return <LyricsStatic>sequelize.define('Lyrics', {
         kor:{
             type: DataTypes.TEXT,
-            allowNull: false,
+            allowNull: false
         },
         eng :{
             type: DataTypes.TEXT,
@@ -16,8 +33,8 @@ module.exports = (sequelize, DataTypes) => {
             type: DataTypes.TEXT,
             allowNull: false,
         }
-    }{
+    }, {
         freezeTableName: true,
-        timesstamps: false
+        timestamps: false
     });
-};
+} 
