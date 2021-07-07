0bd14c534029a271dda22b58fba8cd42b1e5314c
Kayoung <rkdkdudjd@naver.com>
2021-07-07 21:58:07 +0900

    fix: DB 비밀번호 추가

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,7 +4,7 @@ dotenv.config();
 
 export default {
   development: {
-    username: process.env.DB_USERNAME || 'root',
+    username: process.env.DB_USER_NAME || 'admin',
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DBNAME || 'kyrics-development',
     host: process.env.DB_HOST || 'localhost',
@@ -12,7 +12,7 @@ export default {
     dialect: 'mysql',
   },
   production: {
-    username: process.env.DB_USERNAME || 'root',
+    username: process.env.DB_USERNAME || 'admin',
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DBNAME || 'kyrics-production',
     host: process.env.DB_HOST || 'localhost',
