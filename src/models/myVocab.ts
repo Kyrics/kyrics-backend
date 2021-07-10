f0d8c1e4980c07d8e03367109644fbb508fd0118
Cheongha <sch7188@gmail.com>
2021-07-10 22:04:25 +0900

    feat: user, key_ex, mood association
    그리고 관계 테이블(myVocab) 설정

---
+++ src/models/myVocab.ts
@@ -0,0 +1,14 @@
+import { Column, ForeignKey, Table, Model } from 'sequelize-typescript';
+import User from './user';
+import KeyExpression from './keyExpression';
+
+@Table({ tableName: 'my_vocab', freezeTableName: true, underscored: true })
+export default class MyVocab extends Model<MyVocab> {
+  @ForeignKey(() => User)
+  @Column
+  userId: number;
+
+  @ForeignKey(() => KeyExpression)
+  @Column
+  keyExpressionId: number;
+}
