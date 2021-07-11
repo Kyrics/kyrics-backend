04456df0b09f0ecc405b49413f0df7a676954f04
Kayoung Yoon <61582017+kyY00n@users.noreply.github.com>
2021-07-11 23:43:27 +0900

    fix: model attr types (#20)
    mood와 user의 attr 타입을 수정했어요

--- src/models/mood.ts
+++ src/models/mood.ts
@@ -1,23 +1,15 @@
-import {
-  Model,
-  Column,
-  Table,
-  DataType,
-  Unique,
-  BelongsToMany,
-  PrimaryKey
-} from 'sequelize-typescript';
+import { Model, Column, Table, DataType, Unique, BelongsToMany, PrimaryKey } from 'sequelize-typescript';
 import Song from './song';
 import SongMood from './songMood';
 
-@Table({ tableName: 'mood', freezeTableName: true, underscored: true})
+@Table({ tableName: 'mood', freezeTableName: true, underscored: true })
 export default class Mood extends Model<Mood> {
   @PrimaryKey
   @Column
   id: number;
 
   @Unique
-  @Column(DataType.TEXT)
+  @Column(DataType.CHAR(50))
   type: string;
 
   @BelongsToMany(() => Song, () => SongMood)
