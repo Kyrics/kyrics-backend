a38e28eda6f9d08b19dc1c986ff677384b3e8847
Kayoung <rkdkdudjd@naver.com>
2021-07-14 06:47:52 +0900

    chore:  add ai obtion in all models
    auto incrementation

--- src/models/mood.ts
+++ src/models/mood.ts
@@ -1,10 +1,11 @@
-import { Model, Column, Table, DataType, Unique, BelongsToMany, PrimaryKey } from 'sequelize-typescript';
+import { Model, Column, Table, DataType, Unique, BelongsToMany, PrimaryKey, AutoIncrement } from 'sequelize-typescript';
 import Song from './song';
 import SongMood from './songMood';
 
 @Table({ tableName: 'mood', freezeTableName: true, underscored: true, timestamps: false })
 export default class Mood extends Model {
   @PrimaryKey
+  @AutoIncrement
   @Column
   id: number;
 
