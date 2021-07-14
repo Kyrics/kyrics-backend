ade41994eeb0f22ec5046d3e77408e09b1c57ab4
Kayoung <rkdkdudjd@naver.com>
2021-07-14 16:00:00 +0900

    feat: 소셜로그인
    api 만들고 서비스 설계했어요

---
+++ src/controller/signIn.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from 'express';
+import statusCode from '../module/statusCode';
+import { signupIfUserNotFoundAndLogin, socialLoginInput } from '../service/social';
+
+const socialLogin = async (req: Request, res: Response) => {
+  const { name, socialId, email, profileImageUrl, socialType } = req.body;
+  if (!name || !socialId || !socialType) {
+    return res.json({
+      status: 400,
+      message: '필요한 값이 없습니다.',
+    });
+  }
+  const input: socialLoginInput = {
+    name,
+    socialId,
+    email,
+    profileImageUrl,
+    socialType,
+  };
+  try {
+    const jwtAfterLogin = await signupIfUserNotFoundAndLogin(input);
+    return res.json({
+      status: statusCode.OK,
+      data: {
+        socialType,
+        token: jwtAfterLogin,
+      },
+      message: '소셜 로그인 성공',
+    });
+  } catch (error) {
+    console.error(error);
+    return res.json({
+      status: statusCode.INTERNAL_SERVER_ERROR,
+      message: '서버 내부 오류',
+    });
+  }
+};
+
+export { socialLogin };
