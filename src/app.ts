a2ff9e743cae14d2690dfa1a13b4c5dec620a23f
Kayoung <rkdkdudjd@naver.com>
2021-07-14 19:06:16 +0900

    feat: cors

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import path from 'path';
 import YAML from 'yamljs';
 import swaggerUI from 'swagger-ui-express';
+import cors from 'cors';
 import sequelize from './models';
 import router from './router';
 
@@ -14,6 +15,14 @@ sequelize.sync({ force: false }).catch((error) => {
 
 const app = express();
 const swaggerSpec = YAML.load(path.join(__dirname, '../build/swagger.yaml'));
+
+app.use(cors());
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, content-type, x-access-token');
+  next();
+});
 
 app.use(express.urlencoded());
 app.use(express.json());
