a07a39de346c88c1bf6d59419cf03dde3f4993e2
Kayoung <rkdkdudjd@naver.com>
2021-07-14 23:44:43 +0900

    feat: getArtist api

---
+++ src/service/artist.ts
@@ -0,0 +1,37 @@
+import { QueryTypes } from 'sequelize';
+import sequelize from '../models';
+import Artist from '../models/artist';
+
+interface IReadArtistRes {
+  id: number;
+  name: string;
+  backgroundImageUrl: string;
+  logo: string;
+  songs: any;
+}
+
+const readArtist = async (artistId: number): Promise<IReadArtistRes | Error> => {
+  const findArtistRes = await Artist.findByPk(artistId, {
+    attributes: ['id', 'name', 'background_image_url', 'logo_image_url'],
+  });
+  if (!findArtistRes) {
+    throw Error('해당 id의 가수가 없습니다.');
+  }
+  const query = `
+  SELECT song.id, title, album_image_url asalbumImageUrl, artist.name as artist FROM song
+  JOIN album ON(song.album_id=album.id)
+  JOIN song_artist ON(song.id=song_artist.song_id)
+  JOIN artist ON(artist.id=song_artist.artist_id)
+  WHERE artist.id=${artistId}
+  `;
+  const findSongWithAlbumCover = await sequelize.query(query, { type: QueryTypes.SELECT });
+  return {
+    id: artistId,
+    name: findArtistRes.name,
+    backgroundImageUrl: findArtistRes.backgroundImageUrl,
+    logo: findArtistRes.logoImageUrl,
+    songs: findSongWithAlbumCover,
+  };
+};
+
+export { readArtist };
