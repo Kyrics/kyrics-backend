bcb7023812c1ccf2548b1b3608744a0a0fcd1b28
Kayoung <rkdkdudjd@naver.com>
2021-07-14 15:12:04 +0900

    feat: 소셜로그인
    소셜로그인 구현

---
+++ src/service/social.ts
@@ -0,0 +1,38 @@
+import { User } from '../models';
+import { jwtSign } from '../module/jwt';
+
+type SocialType = 'Google' | 'Facebook';
+
+interface socialLoginInput {
+  name: string;
+  socialId: string;
+  email?: string;
+  profileImageUrl?: string;
+  socialType: SocialType;
+}
+
+const signupIfUserNotFoundAndLogin = async (input: socialLoginInput) => {
+  const { name, socialId, email, profileImageUrl, socialType } = input;
+  const userRes = await User.findOne({
+    where: {
+      socialId,
+    },
+  });
+  if (!userRes) {
+    // 회원가입 먼저.
+    await User.create({
+      name,
+      socialId,
+      email,
+      profileImageUrl,
+      socialType,
+    });
+  }
+  const signinInput = {
+    socialId,
+  };
+  const jwtSignRes = await jwtSign(signinInput);
+  return jwtSignRes;
+};
+
+export { SocialType, socialLoginInput, signupIfUserNotFoundAndLogin };
