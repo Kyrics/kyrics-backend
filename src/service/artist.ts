37b3a62f2a58b85c386826595d9f28ff6b5f66e6
Cheongha <sch7188@gmail.com>
2021-07-16 05:51:08 +0900

    fix: getArtistById as backgroundImageUrl
    background_image_url을 bacgroundImageUrl로 바꿔줌

--- src/service/artist.ts
+++ src/service/artist.ts
@@ -11,18 +11,18 @@ interface IReadArtistRes {
 
 const readArtist = async (artistId: number): Promise<IReadArtistRes | Error> => {
   const findArtistRes = await Artist.findByPk(artistId, {
-    attributes: ['id', 'name', 'background_image_url'],
+    attributes: ['id', 'name', ['background_image_url', 'backgroundImageUrl'], ['logo_image_url', 'logoImageUrl']],
   });
   if (!findArtistRes) {
     throw Error('해당 id의 가수가 없습니다.');
   }
-  const query = `
-  SELECT song.id, title, album_image_url as albumImageUrl, artist.name as artist FROM song
-  JOIN album ON(song.album_id=album.id)
-  JOIN song_artist ON(song.id=song_artist.song_id)
-  JOIN artist ON(artist.id=song_artist.artist_id)
-  WHERE artist.id=${artistId}
-  LIMIT 5
+  const query = `SELECT song.id, title, album_image_url as albumImageUrl, artist.name as artist
+                FROM song
+                JOIN album ON(song.album_id=album.id)
+                JOIN song_artist ON(song.id=song_artist.song_id)
+                JOIN artist ON(artist.id=song_artist.artist_id)
+                WHERE artist.id=${artistId}
+                LIMIT 5;
   `;
   const findSongWithAlbumCover = await sequelize.query(query, { type: QueryTypes.SELECT });
   return {
