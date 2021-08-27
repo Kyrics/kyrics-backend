dc23f268dbf118ca5f06abf113dc88cf76326fe0
kayoung yoon <rkdkdudjd@gmail.com>
2021-08-28 02:51:29 +0900

    refac: user 엔티티 리포지토리 추가

--- src/repository/user.ts
+++ src/repository/user.ts
@@ -1,26 +1,49 @@
 import User from '../models/user';
+import { SocialType, userInfoDto } from '../service/social';
 
-const findUserById = async (id: number): Promise<User|null> => {
+const createUser = async (input: userInfoDto): Promise<User> => {
+  const { name, socialId, email, profileImageUrl, socialType } = input;
+  const createUserRes = await User.create({
+    name,
+    socialId,
+    email,
+    profileImageUrl,
+    socialType,
+  });
+  return createUserRes;
+};
+
+const findUserById = async (id: number): Promise<User> => {
   const readUserRes = await User.findOne({
     where: {
-      id
+      id,
     },
-    attributes: ['id', `name`, `email`, `profileImageUrl`]
-  })
+    attributes: ['id', `name`, `email`, `profileImageUrl`],
+  });
   return readUserRes;
-}
+};
+
+const findUserBySocialAccount = async (socialId: string, socialType: SocialType): Promise<User> => {
+  const userRes = await User.findOne({
+    where: {
+      socialId,
+      socialType,
+    },
+  });
+  return userRes;
+};
 
 const deleteUserById = async (id: number): Promise<number> => {
   const destroyUserRes = await User.destroy({
     where: {
-      id
-    }
+      id,
+    },
   });
   return destroyUserRes;
-}
+};
 
 const updateUser = async (user: User): Promise<User> => {
   return user.save();
-}
+};
 
-export { findUserById, deleteUserById, updateUser };
+export { createUser, findUserById, findUserBySocialAccount, deleteUserById, updateUser };
