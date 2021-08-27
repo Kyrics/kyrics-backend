09f62d77c18bd3fac631ec7ef4eb77dad29673be
kayoung yoon <rkdkdudjd@gmail.com>
2021-08-28 02:53:02 +0900

    feat: myVocab 엔티티 리포지토리 추가

--- src/repository/myVocabs.ts
+++ src/repository/myVocabs.ts
@@ -1,28 +1,28 @@
-import { QueryTypes } from "sequelize";
+import { QueryTypes } from 'sequelize';
 import MyVocab from '../models/myVocab';
-import sequelize from "../models";
-import { IMyVocabRes } from "../service/user";
+import sequelize from '../models';
+import { IMyVocabRes } from '../service/user';
 
-const readMyVocabByUserId = async (userId: number): Promise<IMyVocabRes[]|Error> => {
+const readMyVocabByUserId = async (userId: number): Promise<IMyVocabRes[]> => {
   const readMyVocabQuery = `
   SELECT my_vocab.key_expression_id as id, key_expression.kor, key_expression.eng, key_expression.kor_example as korExample, key_expression.eng_example as engExample
   FROM my_vocab
   LEFT OUTER JOIN key_expression ON (my_vocab.key_expression_id = key_expression.id)
   WHERE my_vocab.user_id = ${userId}
   ORDER BY my_vocab.created_at DESC;`;
-  const readMyVocabRes = await sequelize.query(readMyVocabQuery, { type: QueryTypes.SELECT }) as IMyVocabRes[];
+  const readMyVocabRes = (await sequelize.query(readMyVocabQuery, { type: QueryTypes.SELECT })) as IMyVocabRes[];
   return readMyVocabRes;
-}
+};
 
-const createMyVocabByUserId = async (id: number, userId: number): Promise<MyVocab|Error> => {
+const createMyVocabByUserId = async (id: number, userId: number): Promise<MyVocab> => {
   const createMyVocabRes = await MyVocab.create({
     userId,
     keyExpressionId: id,
   });
   return createMyVocabRes;
-}
+};
 
-const destroyMyVocab = async (id: number, userId: number): Promise<number|Error> => {
+const destroyMyVocab = async (id: number, userId: number): Promise<number> => {
   const destroyMyVocabRes = await MyVocab.destroy({
     where: {
       userId,
@@ -30,5 +30,5 @@ const destroyMyVocab = async (id: number, userId: number): Promise<number|Error>
     },
   });
   return destroyMyVocabRes;
-}
+};
 export { readMyVocabByUserId, createMyVocabByUserId, destroyMyVocab };
--- src/service/user.ts
+++ src/service/user.ts
@@ -5,21 +5,21 @@ import { deleteUserById, findUserById, updateUser } from '../repository/user';
 import { readMySongsByUserId, createMySong as createMySongRepo, destroyMySong } from '../repository/mySongs';
 import { createMyVocabByUserId, destroyMyVocab, readMyVocabByUserId } from '../repository/myVocabs';
 
-interface IUserRes{
+interface IUserRes {
   id: number;
   name: string;
   email: string;
   profileImageUrl: string;
 }
 
-interface IMySongsRes{
+interface IMySongsRes {
   id: number;
-	title: string;
-	artists: string[];
-	albumImageUrl: string;
+  title: string;
+  artists: string[];
+  albumImageUrl: string;
 }
 
-interface IMyVocabRes{
+interface IMyVocabRes {
   id: number;
   kor: string;
   eng: string;
@@ -27,62 +27,87 @@ interface IMyVocabRes{
   EngExample: string;
 }
 
-const readUser = async(userId: number): Promise<IUserRes|Error> => {
+const readUser = async (userId: number): Promise<IUserRes> => {
   const readUserRes: IUserRes = await findUserById(userId);
   if (!readUserRes) {
-    return new Error('해당 id의 유저가 없습니다.');
+    throw new Error('해당 id의 유저가 없습니다.');
   }
   return readUserRes;
-}
+};
 
-const deleteUser = async (userId: number): Promise<number|Error> => {
+const deleteUser = async (userId: number): Promise<number> => {
   const destroyUserByIdRes = await deleteUserById(userId);
   if (!destroyUserByIdRes) {
-    return new Error('이미 존재하지 않는 유저입니다.');
+    throw new Error('이미 존재하지 않는 유저입니다.');
   }
   return destroyUserByIdRes;
 };
 
-const updateUserEmail = async (userId: number, newEmail: string): Promise<User|Error> => {
+const updateUserEmail = async (userId: number, newEmail: string): Promise<User> => {
   const findUserRes = await findUserById(userId);
   if (!findUserRes) {
-    return Error('유효하지 않은 아이디');
+    throw Error('유효하지 않은 아이디');
   }
   findUserRes.email = newEmail;
   const updateUserEmailRes = await updateUser(findUserRes);
   return updateUserEmailRes;
 };
 
-const readMySongs = async(userId: number): Promise<IMySongsRes[] | Error> => {
+const readMySongs = async (userId: number): Promise<IMySongsRes[]> => {
   return readMySongsByUserId(userId);
-}
-
-const createMySong = async (id: number, userId: number): Promise<MySongs | Error> => {
-  const createMySongRes = await createMySongRepo(id, userId);
-  return createMySongRes;
 };
 
-const deleteMySong = async (id: number, userId: number): Promise<number | Error> => {
+const createMySong = async (id: number, userId: number): Promise<MySongs> => {
+  try {
+    const createMySongRes = await createMySongRepo(id, userId);
+    return createMySongRes;
+  } catch (error) {
+    if (error.message === 'Validation error') throw new Error('이미 MySongs에 존재하는 노래입니다.');
+    throw error;
+  }
+};
+
+const deleteMySong = async (id: number, userId: number): Promise<number> => {
   const destroyMySongRes = await destroyMySong(id, userId);
-  if(!destroyMySong) {
-    return new Error('MySongs에 존재하지 않는 노래입니다.');
+  if (!destroyMySongRes) {
+    throw new Error('MySongs에 존재하지 않는 노래입니다.');
   }
   return destroyMySongRes;
 };
 
-const readMyVocab = async(userId: number): Promise<IMyVocabRes[] | Error> => {
-  return readMyVocabByUserId(userId);
-}
+const readMyVocab = async (userId: number): Promise<IMyVocabRes[]> => {
+  const readMyVocabRes = await readMyVocabByUserId(userId);
+  return readMyVocabRes;
+};
 
-const createMyVocab = async (id: number, userId: number): Promise<MyVocab | Error> => {
-    return createMyVocabByUserId(id, userId);
-  };
-  
-  const deleteMyVocab = async (id: number, userId: number): Promise<number | Error> => {
-    if (!deleteMyVocab) {
-      return new Error('myVocab에 존재하지 않는 vocab입니다.');
-    }
-    return destroyMyVocab(id, userId);
-  };
+const createMyVocab = async (id: number, userId: number): Promise<MyVocab> => {
+  try {
+    const createMyVocabRes = await createMyVocabByUserId(id, userId);
+    return createMyVocabRes;
+  } catch (error) {
+    if (error.message === 'Validation error') throw new Error('이미 MyVocab에 존재하는 단어입니다.');
+    throw error;
+  }
+};
 
-export { readUser, deleteUser, updateUserEmail, readMySongs, createMySong, deleteMySong, readMyVocab, createMyVocab, deleteMyVocab, IMySongsRes, IMyVocabRes };
+const deleteMyVocab = async (id: number, userId: number): Promise<number> => {
+  const deletedRowNum = await destroyMyVocab(id, userId);
+  if (!deletedRowNum) {
+    throw new Error('myVocab에 존재하지 않는 vocab입니다.');
+  }
+  return deletedRowNum;
+};
+
+export {
+  readUser,
+  deleteUser,
+  updateUserEmail,
+  readMySongs,
+  createMySong,
+  deleteMySong,
+  readMyVocab,
+  createMyVocab,
+  deleteMyVocab,
+  IMySongsRes,
+  IMyVocabRes,
+};
